ccc3486981bd43348d12e738a68bf8f13dd0450d
Vlad Gerula <vlad.gerula@gmail.com>
2025-05-04 18:07:18 +0300

    trying to deploy

--- Lab5/vite.config.ts
+++ Lab5/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TypeScript-PNU/',
+  base: '/TypeScript-PNU/Lab5/',
   plugins: [react()],
 })
