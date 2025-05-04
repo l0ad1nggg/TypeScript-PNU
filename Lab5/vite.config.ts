e46ae8307a5dd51cb2c18904c868717658d5d054
Vlad Gerula <vlad.gerula@gmail.com>
2025-05-04 18:11:17 +0300

    trying to deployy

--- Lab5/vite.config.ts
+++ Lab5/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.ts
 export default defineConfig({
   base: '/TypeScript-PNU/Lab5/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
 })
